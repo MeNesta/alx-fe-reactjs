538d2f13569125d246bee38ca3b82bb2e8a3d8e6
Edward Ofori <oforikofi.edward@gmail.com>
2025-09-07 13:01:41 +0000

    add userprofile.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,30 +1,17 @@
-import './App.css'
-import Header from './components/Header'
-import MainContent from './components/Maincontent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'  // ← Add this import
-import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
+import UserProfile from './UserProfile';
+
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  };
+
   return (
-    <>
-      <Header />
-      <MainContent />
-      
-      {/* Add the UserProfile component with props */}
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-      <UserContext.Provider value={userData}>
-      <ProfilePage />
+    <UserContext.Provider value={userData}>
+      <UserProfile />
     </UserContext.Provider>
-    
-      <Footer />
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
