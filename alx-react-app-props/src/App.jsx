43d4feabee855f111221ec5f908942183253674e
Edward Ofori <oforikofi.edward@gmail.com>
2025-09-07 13:14:26 +0000

    chnages to all jsx files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,17 @@
-import ProfilePage from './profilePage';
+import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
