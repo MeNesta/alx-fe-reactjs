a31d61584fdd5de93f1ff810bd2cfdf374565057
Edward Ofori <oforikofi.edward@gmail.com>
2025-09-07 13:53:01 +0000

    changes

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,15 @@
-const UserProfile = (props) => {
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // Correct relative path
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-   <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-   <h2 style={{ color: 'blue' }}>{props.name}</h2>
-   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-   <p>Bio: {props.bio}</p>
- </div>
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Email: {userData.email}</p>
+    </div>
   );
-};
+}
 
 export default UserProfile;
